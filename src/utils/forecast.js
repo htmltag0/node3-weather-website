12ff4fdbdb158e709144579e7141a1bdc459644a
iykang85 <iyk.softmania@gmail.com>
2019-11-15 18:17:02 +0900

    Change forecast message

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,7 +1,8 @@
 const request = require('request')
 
 const forecast = (longitude, latitude, callback) => {
-    const url = 'https://api.darksky.net/forecast/d6956dc1d54b7eb0afc30f419902313a/' + latitude + ',' + longitude + '?unit=si'
+    const url = 'https://api.darksky.net/forecast/d6956dc1d54b7eb0afc30f419902313a/' + latitude + ',' + longitude + '?units=si'
+    console.log(url)
 
     request({ url, json: true }, (error, { body })=>{
         if (error) {
@@ -9,7 +10,8 @@ const forecast = (longitude, latitude, callback) => {
         } else if (body.error) {
             callback(body.error, {})
         } else {
-            callback(undefined, body.currently.summary + '. It is currently ' + body.currently.temperature + ' degrees out. ' +
+            callback(undefined, body.daily.data[0].summary + '. It is currently ' + body.currently.temperature + ' degrees out. ' +
+                'Temperature high is at ' + body.daily.data[0].temperatureHigh + ' degrees and low is at ' + body.daily.data[0].temperatureLow + ' degrees. ' + 
                 'There is a ' + body.currently.precipProbability + '% chance of rain.')
         }
     })
